596c525689ea810980956ae20d1a7216628b8c30
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-02-11 21:32:31 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,14 +3,14 @@ class Application
  def call(env)
    resp = Rack::Response.new 
    
-   time = Time.now.hour 
-   
-   if time => 12
-     resp.write "Good Morning!"
-   else 
-       resp.write "Good Afternoon!"
- end 
- resp.finish 
-  end 
- end 
-end 
+   time = Time.now.hour
+
+    if time <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end 
+    resp.finish
+  end
+
+end
